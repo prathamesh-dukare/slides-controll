07f02cb4244aafe471145d5fab66aad4b4f9923f
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2024-12-18 22:06:33 +0530

    style : add shadcn components

---
+++ client/src/components/carousel.tsx
@@ -0,0 +1,27 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export default function CarouselComponent() {
+  return (
+    <Carousel>
+      <CarouselContent>
+        <CarouselItem>
+          <div className="  bg-red-500 block w-14 h-14"></div>
+        </CarouselItem>
+        <CarouselItem>
+          <div className="  bg-blue-500 block w-14 h-14"></div>
+        </CarouselItem>
+        <CarouselItem>
+          <div className="  bg-green-500 block w-14 h-14"></div>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
---
+++ client/src/components/layout/Container.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+export function Container({ children, className, ...props }: ContainerProps) {
+  return (
+    <div
+      className={cn("container mx-auto px-4 py-8 max-w-7xl", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
